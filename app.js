0975cd255b612583b9efd22b03e04857675fbf9c
Aswin <106148294+aswin0110@users.noreply.github.com>
2023-03-01 12:11:38 +0530

    crud added

--- app.js
+++ app.js
@@ -2,32 +2,147 @@ const express = require('express');
 const app = new express();
 const logger = require('morgan')
 const cors = require('cors')
+const color = require('colors')
+
+
+
+const bodyParser = require('body-parser');
+
+
 
 require('dotenv').config()
 
+
 const PORT = process.env.PORT
+
 
 app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))
 app.use(logger('dev')) 
 app.use(cors())
 
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.json());
+
 const data = require('./data/data.json')
 
 
-//Path using fs
+
 const fs =require('fs')
-fs.readdir('data',(err,res) =>{
-    if(err){
-        console.log('Path not found');
-    }
-    else{
-        console.log('The directory is: '+ res)
-    }
-})
+
+//Path using fs
+
+
+
+//read the json file using fs.readFile
+
+// fs.readFile('./data/data.json','utf-8',(err,res) =>{
+
+//     if (err){
+//         console.log('Something went wrong here '.red+err);
+//     }
+//     else{
+//         try {
+//             const hospital = JSON.parse(res);
+//             const location = hospital.map(hospital =>hospital.H_Location);
+//             // console.log(res);
+//             console.log('Hospital Locations: '.green+ location);
+    
+//         } catch (err) {
+//             console.log('Error parsing JSON '.red,err)
+            
+//         }
+
+//     }
+// })
+
+
+
+
+// app.put('/api/create', (req, res) => {
+//     const { id, Hospital_Name, Patient_Count, H_Location } = req.body;
+//     const newData = {
+//     id,
+//     Hospital_Name,
+//     Patient_Count,
+//     H_Location
+// };
+
+
+
+// fs.appendFile('./data/newJSON.json',JSON.stringify(newData, null, 2), err =>{
+//     if(err){
+//         console.log(err);
+
+//     }
+//     else{
+//         console.log('File Successfully written!'.green);
+//         res.send('done')
+//     }
+// })
+
+// });
+  app.delete('/api/delete', (req,res) =>{
+    const filePath = './data/newJSON.json';
+    fs.access(filePath, error => {
+        if (!error) {
+        fs.unlink(filePath,function(error){
+        if(error) console.error('Error Occured:', error);
+            console.log('File deleted!');
+            res.send('Deleted Successfully');
+        });
+        } else {
+            console.error('Error Occured:', error);
+        }
+        
+    });
+  })
+
+
+
+
+
+//Creating a json file and add datas
+// const newData = {
+//     id: 1,
+//     Hospital_Name: 'Hospital D',
+//     Patient_Count: 210,
+//     H_Location: 'TVM'
+// };
+
+
+// fs.writeFile('./data/newData.json',JSON.stringify(newData, null, 2), err =>{
+//     if(err){
+//         console.log(err);
+
+//     }
+//     else{
+//         console.log('File Successfully written!'.green);
+//     }
+// })
+
+// end 
+
+
+// add a text
+// const newData1 = {
+//     id: 5,
+//     Hospital_Name: 'Hospital E',
+//     Patient_Count: 20,
+//     H_Location: '"ALPY'
+// };
+
+// fs.appendFile("./data/newData.json",newData1,function(error){
+//     if (error){
+//         console.log('unable to write')
+//     }
+//     else{
+//         console.log('done append working');
+//     }
+// });
 
 var result = fs.readdirSync('data');
-console.log('Read output is ' + result);
+// console.log('Read output is '.green + result);
 // end
 
 
@@ -39,9 +154,15 @@ console.log('Read output is ' + result);
 const welcome =  require('./routes/welcome')
 app.use('/',welcome)
 
+
 //main page with json page: /main
 const main = require('./routes/main')
 app.use('/main',main)
+
+//create a  json using post and save a file in ./data folder
+const create = require('./routes/create')
+app.use('/create',create)
+
 
 //get single data from json by district /district/Kannur 
 app.get('/class/:H_Location',(req,res) => {
@@ -64,16 +185,7 @@ app.get('/class/:H_Location',(req,res) => {
 
 
 
-
-
-
-
-
-
-
-
-
 app.listen(PORT, ()=>{
-    console.log(`Server is running in ${PORT}`);
-    console.log(data) //json file
+    console.log(`Server is running in ${PORT}`.black.bgWhite.underline);
+    //console.log(data) //json file
 })
---
+++ routes/create.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const router = express.Router();
+const fs =require('fs')
+
+//GET
+router.get('/',(req,res) =>{
+
+    const a = res.send('hai')
+    console.log(`${a} shs`.yellow);
+
+    
+    fs.readdir('data',(err,res) =>{
+        if(err){
+            console.log('Path not found');
+        }
+        else{
+            console.log('The directory is: '.yellow+ res)
+        }
+    })
+});
+
+
+//CREATE A JSON FILE USING .post and writeFile
+//write to create a json file and add json's through body in postman 
+
+router.post('/', (req, res) => {
+    const { id, Hospital_Name, Patient_Count, H_Location } = req.body;
+    const newData = {
+        id,
+        Hospital_Name, 
+        Patient_Count, 
+        H_Location
+    };
+  
+    res.json(newData);
+
+    fs.writeFile('./data/newJSON.json',JSON.stringify(newData, null, 2), err => {
+        if(err) {
+
+            console.log(err);
+    
+        }
+        else{
+            let jsonfile = require('./data/newJSON.json');
+            res.send(jsonfile)
+            console.log('newJSON.json File Successfully written!'.yellow);
+        }
+
+    });
+
+});
+
+// add new data's to the created json file
+
+
+
+//delete created json
+router.delete('/', (req,res) =>{
+    const filePath = './data/newJSON.json';
+    fs.access(filePath, error => {
+        if (!error) {
+        fs.unlink(filePath,function(error){
+        if(error) console.error('Error Occured:', error);
+            console.log('File deleted!'.red);
+            res.send('Deleted Successfully');
+        });
+        } else {
+            console.error('Error Occured:', error);
+        }
+        
+    });
+  })
+
+
+
+
+
+
+module.exports = router
